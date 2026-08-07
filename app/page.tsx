56b6ee7da411229f94f33d01e3c401ebcec0f014
Matheus Lufano <matheussantosmota1910@gmail.com>
2026-08-07 15:12:12 -0300

    correção da Sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1216,10 +1216,7 @@ export default function Home() {
       >
         <small>Atendimento agora!</small>
         <strong>
-          <svg viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8Z" />
-            <path d="M9.1 8.7c.2 2.3 2 4.1 4.3 4.4m-.1 0 1.5-.8 1.4 1.1-.4 1.5c-.2.5-.7.7-1.2.6-3.3-.7-5.9-3.3-6.6-6.6-.1-.5.1-1 .6-1.2l1.5-.4 1.1 1.4-.8 1.5" />
-          </svg>
+          <img src="/whatsapp-floating.png" alt="" aria-hidden="true" />
           <span>Fale Conosco</span>
         </strong>
       </a>
